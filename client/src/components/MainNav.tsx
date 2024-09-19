5c84a0e8d2c1edab09c56919b85fecb3caaa1fdb
Adedeji Bisade <dejobs247@yahoo.com>
2024-09-19 19:16:18 +0100

    setup Auth0 for client side and initiate login

--- client/src/components/MainNav.tsx
+++ client/src/components/MainNav.tsx
@@ -1,10 +1,13 @@
 import {Button} from "./ui/button";
+import {useAuth0} from "@auth0/auth0-react";
 
 const MainNav = () => {
+  const {loginWithRedirect} = useAuth0();
   return (
     <Button
       variant="ghost"
       className="font-bold hover:bg-white hover:text-red-600"
+      onClick={() => loginWithRedirect()}
     >
       Login
     </Button>
--- client/src/components/MobileNav.tsx
+++ client/src/components/MobileNav.tsx
@@ -8,8 +8,10 @@ import {
   SheetDescription,
 } from "./ui/sheet";
 import {Menu} from "lucide-react";
+import {useAuth0} from "@auth0/auth0-react";
 
 const MobileNav = () => {
+  const {loginWithRedirect} = useAuth0();
   return (
     <Sheet>
       <SheetTrigger>
@@ -21,7 +23,12 @@ const MobileNav = () => {
         </SheetTitle>
         <Separator />
         <SheetDescription className="flex">
-          <Button className="font-bold flex-1 bg-orange-500">Log In</Button>
+          <Button
+            className="font-bold flex-1 bg-orange-500"
+            onClick={() => loginWithRedirect()}
+          >
+            Log In
+          </Button>
         </SheetDescription>
       </SheetContent>
     </Sheet>
